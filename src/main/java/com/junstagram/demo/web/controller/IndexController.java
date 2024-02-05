76309c81bd3bab1eca23c8fb81c090cbf77b2629
bjl0615 <bjl0615@gmail.com>
2024-02-05 23:42:26 +0900

    CSS && JS && HTML

--- src/main/java/com/junstagram/demo/web/controller/IndexController.java
+++ src/main/java/com/junstagram/demo/web/controller/IndexController.java
@@ -1,4 +1,14 @@
 package com.junstagram.demo.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
 }
