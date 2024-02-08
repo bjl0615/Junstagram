47fcbd2d91587a15775621e3b7019ff565c757b8
bjl0615 <bjl0615@gmail.com>
2024-02-08 18:49:45 +0900

    Likes Service

--- src/main/java/com/junstagram/demo/service/LikeService.java
+++ src/main/java/com/junstagram/demo/service/LikeService.java
@@ -1,4 +1,29 @@
 package com.junstagram.demo.service;
 
+import com.junstagram.demo.handler.exception.CustomApiException;
+import com.junstagram.demo.repository.LikesRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class LikeService {
+
+    private final LikesRepository likesRepository;
+
+    @Transactional
+    public void likes(Long postId , Long sessionId) {
+        try{
+            likesRepository.likes(postId , sessionId);
+        }catch (Exception e) {
+            throw new CustomApiException("이미 좋아요를 누르셨습니다.");
+        }
+    }
+
+    @Transactional
+    public void unlikes(Long postId , Long sessionId) {
+        likesRepository.unLikes(postId , sessionId);
+    }
+
 }
