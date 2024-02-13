4e25c1ecba97bade39d451ba5fcde7f174161fd9
bjl0615 <bjl0615@gmail.com>
2024-02-13 19:08:07 +0900

    Index Controller Implement

---
+++ src/main/java/com/junstagram/demo/handler/aop/ValidationAdvice.java
@@ -0,0 +1,4 @@
+package com.junstagram.demo.handler.aop;
+
+public class ValidationAdvice {
+}
--- src/main/java/com/junstagram/demo/web/controller/IndexController.java
+++ src/main/java/com/junstagram/demo/web/controller/IndexController.java
@@ -1,14 +1,16 @@
 package com.junstagram.demo.web.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
-        return "login";
+    @GetMapping({"/" , "post/story"})
+    public String story(){
+        return "post/story";
     }
 
     @GetMapping("/login")
